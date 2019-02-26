c0ebdd63efac09580f4191b04657801fb5eb8cb6
Danny Martin <danielmartin0126@gmail.com>
2019-02-26 06:24:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,11 +16,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop()
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift()
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -32,5 +32,5 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
